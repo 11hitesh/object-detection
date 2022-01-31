78ed149a8c163fd7b9eff2485113d60542394cb2
11hitesh <72382736+11hitesh@users.noreply.github.com>
2022-01-31 14:57:34 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,41 @@
+img="";
+status="";
+
+function preload() 
+{
+    img=loadImage("desk.jfif");
+}
+
+function setup()
+{
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectdetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="status:detecting objects";
+}
+
+function modelLoaded() 
+{
+console.log("model loaded!");
+status=true;
+objectdetector.detect(img,gotResult);
+}
+
+function gotResult(error,results) 
+{
+if(error)
+{
+    console.error(error);
+}
+console.log(results);
+}
+
+function draw()
+{
+    image(img,0,0,640,420);
+    fill("lightgreen");
+    text("desk",45,75);
+    noFill();
+    stroke("lightgreen");
+    rect(30,60,450,350);
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+function bedroom_page()
+{
+    window.location="bedroom.html";
+}
+
+function tv_page()
+{
+    window.location="tv.html";
+}
+
+function desk_page()
+{
+    window.location="desk.html";
+}
+
+function bottle_page()
+{
+    window.location="bottles.html";
+}
+
+function fruit_page()
+{
+    window.location="fruit.html";
+}
